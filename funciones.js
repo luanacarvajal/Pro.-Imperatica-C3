b25d6c3683f8697c14eb26ebd408c84290bbfb7c
luanacarvajal <luanacarvajalgonzales@gmail.com>
2023-04-02 19:08:00 -0400

    probando

---
+++ funciones.js
@@ -0,0 +1,7 @@
+// es un bloc de codigo, en el cual puedo poner las reglas, 
+
+function sumar (){
+//  bloque de codigo
+
+
+}
---
+++ variables.js
@@ -0,0 +1,41 @@
+
+// Variables -- una caja donde guardamos algo
+
+let nombreUsuario = 'pepito';
+
+const apellido = 'perez';
+
+let edad = 30;
+    edad = 31;
+// string ---> cadenas de texto o caracteres
+let frase = "hola";
+
+
+// number
+
+let cantidadMascsotas = 31323334;
+    cantidadMascsotas = cantidadMascsotas + 1;
+console.log(cantidadMascsotas);
+let precio = 5.40;
+
+// booleanos
+let isLogged = true;
+let estaCasado = false;
+let esMayorDeEdad = false;
+
+// UNDERFINED
+
+let email = undefined;
+
+    email = 'ppito@gmail.com';
+
+    console.log(email);
+
+    // OPERADORES MATEMATICOS
+
+    let nombre = "maria";
+    let apellido2 = "perez";
+
+    let saludo = "hola" + nombre + apellido2 + " como estas?";
+    let saludo2 = "hola ${nombre} ${apellido2} como estas?";
+    ``
