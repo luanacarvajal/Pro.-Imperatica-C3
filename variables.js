9706fe56aa881e7bc453ab50555fe369d5983981
luanacarvajal <luanacarvajalgonzales@gmail.com>
2023-04-02 21:40:13 -0400

    clase2

--- variables.js
+++ variables.js
@@ -13,32 +13,64 @@ let edad = 30;
     edad = 31;
 // string ---> cadenas de texto o caracteres
 let frase = "hola";
+console.log("typeof frase");
+
 
 
 // number
 
-let cantidadMascsotas = 31323334;
+let cantidadMascsotas = 3132;
     cantidadMascsotas = cantidadMascsotas + 1;
-console.log(cantidadMascsotas);
+   
 let precio = 5.40;
+
+console.log("cantidadMascsotas");
+
 
 // booleanos
 let isLogged = true;
 let estaCasado = false;
 let esMayorDeEdad = false;
 
-// UNDERFINED
+// UNDERFINED (es un tipo de dato que no esta definido)
 
 let email = undefined;
 
     email = 'ppito@gmail.com';
 
-    console.log(email);
+    console.log("email");
+    // null
+    // NaN
 
-    // OPERADORES MATEMATICOS
+    // OPERADORES MATEMATICOS ()
 
     let nombre = "maria";
     let apellido2 = "perez";
+    let nombreCompleto = '${nombre} ${apellido2}'
 
-    let saludo = "hola" + nombre + apellido2 + " como estas?";
-    let saludo2 = "hola ${nombre} ${apellido2} como estas?";
+    let saludo = "como estas" + nombre;
+    let saludo2 = "hola" + nombre + apellido2 + "como estas?"
+    console.log( "saludo2" )
+    // + - * / %
+
+    let num = 10
+    let num2 = 2
+
+    console.log(num + num2)
+    console.log(num - mum2)
+    console.log(num * num2)
+    console.log(num / num2)
+    console.log(num % num2)
+
+    console.log( 29 % 3 )
+ // operades de comparaciones ----> develve un booleano
+ console.log("--------------------")
+ let n1 = 12
+ let n2 = 15
+
+ console.log( n1 < n2 )
+ console.log( n1 > n2 )
+ console.log( n1 <= n2 ) 
+ console.log( n1 >= n2 )
+ console.log(n1 == n2 )
+ console.log(n1 != n2 )
